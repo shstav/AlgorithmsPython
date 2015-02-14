08068e51c849eb8bc55e6e149301eb0f1c15a6ec
Stav <stav@ftbpro.com>
2015-02-14 16:40:15 +0200

    BFS

---
+++ bfs.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+"""
+         A
+    B         C
+  D   E     F
+G
+"""
+graph = {
+  'A': set(['B', 'C']),
+  'B': set(['D', 'E']),
+  'C': set(['F']),
+  'D': set(['G'])
+}
+
+def bfs(graph_root, graph):
+  nodes_queue = deque([graph_root])
+  while nodes_queue:
+    current_node = nodes_queue.popleft()
+    print current_node
+    nodes_queue.extend(graph.get(current_node, []))
+
